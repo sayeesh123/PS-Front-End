4c217806990993758bd8e6fa5623202313af310d
Sayeesh123 <92074452+Sayeesh123@users.noreply.github.com>
2022-05-20 15:57:52 +0530

    latest

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# PSProject
+# PSFrontEnd
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 13.1.0-next.2.
 
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'PS-Project'`, () => {
+  it(`should have as title 'PS-Front-End'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('PS-Project');
+    expect(app.title).toEqual('PS-Front-End');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('PS-Project app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('PS-Front-End app is running!');
   });
 });
